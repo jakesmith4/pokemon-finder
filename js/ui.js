373a3a0b4d37ff92c95277b591cc51e8d7e9e020
Jacob Smith <jakesmith.seo@gmail.com>
2022-01-27 01:23:17 -0600

    fixed pokemon cards to WAY BETTER

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,92 @@
 class UI {
   constructor() {
     this.container = document.querySelector('.container');
+    this.container2 = document.querySelector('.container-2');
   }
 
   // Show Pokemon
   showPokemon(pokemon) {
     console.log(pokemon);
     this.container.innerHTML = `
-    <div>
-        <span class="pokenumber">${pokemon.id}</span>
-        <div class="underline"></div>
-        <span class="poketype">${pokemon.types[0].type.name}
-        </span>
-        <div class="img-container">
-        <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
+    <article>
+      <h4>${pokemon.name}</h4>
+      <span class="poke-number">#00${pokemon.id}</span>
+      <span class="poke-type">${pokemon.types[0].type.name}</span>
+      <span class="poke-type-2">${
+        pokemon.types[1] ? pokemon.types[1].type.name : ''
+      }</span>
+      <div class="article-img-container">
+        <img src="${
+          pokemon.sprites.other['official-artwork'].front_default
+        }" class="article-img" alt="${pokemon.name}">
         </div>
-        <h3 class="section-title">${pokemon.name}</h3>
-    </div>
+        <div class="pokemon-stats">
+        <ul class="poke-list">
+        <li>Weight:</li>
+        <li><span class="stats">${pokemon.weight} lbs</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Height:</li>
+        <li><span class="stats">${pokemon.height}</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Abilites:</li>
+        <li><span class="stats">${pokemon.abilities[0].ability.name}</span></li>
+        <li><span class="stats">${
+          pokemon.abilities[1] ? pokemon.abilities[1].ability.name : ''
+        }</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Base Experience:</li>
+        <li><span class="stats">${pokemon.base_experience}</span></li>
+        </ul>
+        </div>
+      </article>
     `;
+
+    this.changeAllBackgrounds(pokemon);
+  }
+  // Change Article Background
+  changeBackground(pokemonType, color, poke, color2) {
+    const article = document.querySelector('article');
+    const stats = document.querySelectorAll('.stats');
+    const pokeType = document.querySelectorAll('.poke-type');
+    const pokeType2 = document.querySelectorAll('.poke-type-2');
+    const type = poke.types[0].type.name;
+    if (type === pokemonType) {
+      article.style.background = color;
+      stats.forEach((stat) => {
+        stat.style.color = color;
+      });
+      pokeType.forEach((type) => {
+        type.style.background = color2;
+      });
+      pokeType2.forEach((type) => {
+        type.style.background = color2;
+      });
+    }
+  }
+
+  // Change Background For All Pokemon
+  changeAllBackgrounds(poke) {
+    this.changeBackground('fire', '#fb6c6c', poke, '#f88c8c');
+    this.changeBackground('electric', '#ffd86f', poke, '#ffe08c');
+    this.changeBackground('bug', '#79ff6f', poke, '#94ff8c');
+    this.changeBackground('water', '#61e0c9', poke, '#81e6d4');
+    this.changeBackground('ice', '#96D9D6', poke, '#abe1de');
+    this.changeBackground('grass', '#7AC74C', poke, '#95d270');
+    this.changeBackground('normal', '#A8A77A', poke, '#b9b995');
+    this.changeBackground('ground', '#E2BF65', poke, '#e8cc84');
+    this.changeBackground('fighting', '#C22E28', poke, '#ce5853');
+    this.changeBackground('poison', '#A33EA1', poke, '#b565b4');
+    this.changeBackground('flying', '#A98FF3', poke, '#baa5f5');
+    this.changeBackground('psychic', '#F95587', poke, '#fa779f');
+    this.changeBackground('rock', '#B6A136', poke, '#c5b45e');
+    this.changeBackground('ghost', '#735797', poke, '#8f79ac');
+    this.changeBackground('dragon', '#6F35FC', poke, '#8c5dfd');
+    this.changeBackground('dark', '#705746', poke, '#8d796b');
+    this.changeBackground('steel', '#B7B7CE', poke, '#c5c5d8');
+    this.changeBackground('fairy', '#D685AD', poke, '#de9dbd');
   }
 
   // Show All Pokemon
@@ -31,15 +100,32 @@ class UI {
         .then((response) => response.json())
         .then((data) => {
           this.container.innerHTML += `
-            <div>
-            <span class="pokenumber">${data.order}</span>
-            <div class="underline"></div>
-            <span class="poketype">${data.types[0].type.name}</span>
-            <div class="img-container">
-            <img src="${data.sprites.other['official-artwork'].front_default}" alt="${data.name}">
-            </div>
-            <h3 class="section-title">${data.name}</h3>
-            </div>
+      <article class="poke-background">
+      <h4>${pokemon.name}</h4>
+      <span class="poke-number">#00${data.order}</span>
+      <span class="poke-type">${data.types[0].type.name}</span>
+      <div class="article-img-container">
+        <img src="${data.sprites.other['official-artwork'].front_default}" class="article-img" alt="${data.name}">
+        </div>
+        <div class="pokemon-stats">
+        <ul class="poke-list">
+        <li>Weight:</li>
+        <li><span>${data.weight} lbs</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Height:</li>
+        <li><span>${data.height}</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Abilites:</li>
+        <li><span>${data.abilities[0].ability.name}</span></li>
+        </ul>
+        <ul class="poke-list">
+        <li>Base Experience:</li>
+        <li><span>${data.base_experience}</span></li>
+        </ul>
+        </div>
+      </article>
             `;
         });
     });
