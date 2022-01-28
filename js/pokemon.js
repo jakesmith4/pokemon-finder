d724ca9f1fa2923a230f879cb360e37299e7e44d
Jacob Smith <jakesmith.seo@gmail.com>
2022-01-27 21:00:15 -0600

    changed show all pokemon button to show only 50 pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -13,7 +13,7 @@ class Pokemon {
         `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
       );
       const allPokemonResponse = await fetch(
-        'https://pokeapi.co/api/v2/pokemon?limit=151'
+        'https://pokeapi.co/api/v2/pokemon?limit=50'
       );
 
       const pokemonData = await pokemonResponse.json();
